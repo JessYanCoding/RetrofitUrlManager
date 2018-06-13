345ab2fb8e94d1d986c194ef4b83ec885fd483ff
JessYan <jessyan@foxmail.com>
2018-06-13 18:02:21 +0800

    Finish DomainUrlParser

--- manager/src/main/java/me/jessyan/retrofiturlmanager/parser/DomainUrlParser.java
+++ manager/src/main/java/me/jessyan/retrofiturlmanager/parser/DomainUrlParser.java
@@ -15,17 +15,24 @@
  */
 package me.jessyan.retrofiturlmanager.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.jessyan.retrofiturlmanager.RetrofitUrlManager;
 import okhttp3.HttpUrl;
 
 /**
  * ================================================
- * 域名解析器, 此解析器用来解析域名, 只会将旧 URL 地址中的域名替换成你想要的域名
+ * 域名解析器, 此解析器用来解析域名, 默认将您的域名作为 BaseUrl, 只会将旧 URL 地址中的域名替换成你想要的地址
  * <p>
  * 比如:
+ * 1.
  * 旧 URL 地址为 https://www.github.com/wiki, 您调用 {@link RetrofitUrlManager#putDomain(String, String)}
- * 方法传入的 URL 地址是 https://www.google.com/api, 经过本解析器解析后生成的新 URL 地址为 http://www.google.com/wiki
- * 在域名后面的地址将被忽略
+ * 方法传入的 URL 地址是 https://www.google.com/api, 经过本解析器解析后生成的新 URL 地址为 http://www.google.com/api/wiki
+ * <p>
+ * 2.
+ * 旧 URL 地址为 https://www.github.com/wiki, 您调用 {@link RetrofitUrlManager#putDomain(String, String)}
+ * 方法传入的 URL 地址是 https://www.google.com, 经过本解析器解析后生成的新 URL 地址为 http://www.google.com/wiki
  *
  * @see UrlParser
  * Created by JessYan on 17/07/2017 18:23
@@ -34,16 +41,36 @@ import okhttp3.HttpUrl;
  * ================================================
  */
 public class DomainUrlParser implements UrlParser {
+
+    @Override
+    public void init(RetrofitUrlManager retrofitUrlManager) {
+
+    }
+
     @Override
     public HttpUrl parseUrl(HttpUrl domainUrl, HttpUrl url) {
-
         // 如果 HttpUrl.parse(url); 解析为 null 说明,url 格式不正确,正确的格式为 "https://github.com:443"
         // http 默认端口 80, https 默认端口 443, 如果端口号是默认端口号就可以将 ":443" 去掉
         // 只支持 http 和 https
 
         if (null == domainUrl) return url;
 
-        return url.newBuilder()
+        HttpUrl.Builder builder = url.newBuilder();
+
+        for (int i = 0; i < url.pathSize(); i++) {
+            //当删除了上一个 index, PathSegment 的 item 会自动前进一位, 所以 remove(0) 就好
+            builder.removePathSegment(0);
+        }
+
+        List<String> newPathSegments = new ArrayList<>();
+        newPathSegments.addAll(domainUrl.encodedPathSegments());
+        newPathSegments.addAll(url.encodedPathSegments());
+
+        for (String PathSegment : newPathSegments) {
+            builder.addEncodedPathSegment(PathSegment);
+        }
+
+        return builder
                 .scheme(domainUrl.scheme())
                 .host(domainUrl.host())
                 .port(domainUrl.port())
