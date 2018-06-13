16eb3ed7141c11a624e735c4c5d809f507e73da7
JessYan <jessyan@foxmail.com>
2018-06-13 12:14:10 +0800

    Add UrlParser#init

--- manager/src/main/java/me/jessyan/retrofiturlmanager/parser/UrlParser.java
+++ manager/src/main/java/me/jessyan/retrofiturlmanager/parser/UrlParser.java
@@ -30,12 +30,20 @@ import okhttp3.Request;
  */
 
 public interface UrlParser {
+
+    /**
+     * 这里可以做一些初始化操作
+     *
+     * @param retrofitUrlManager {@link RetrofitUrlManager}
+     */
+    void init(RetrofitUrlManager retrofitUrlManager);
+
     /**
      * 将 {@link RetrofitUrlManager#mDomainNameHub} 中映射的 URL 解析成完整的{@link HttpUrl}
      * 用来替换 @{@link Request#url} 达到动态切换 URL
      *
      * @param domainUrl 用于替换的 URL 地址
-     * @param url 旧 URL 地址
+     * @param url       旧 URL 地址
      * @return
      */
     HttpUrl parseUrl(HttpUrl domainUrl, HttpUrl url);
